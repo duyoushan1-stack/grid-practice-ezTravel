2fc31c45f72af533aa4016155158e582c00c7124
sandy <duyoushan1@gmail.com>
2026-05-10 23:51:35 +0800

    feat: 新增自遊最夯區塊，增添分頁切換功能

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 const tourList = document.querySelector('.tour-list')
 const tourContent = document.querySelector('.tour-content')
-// DOM 陣列
-const liArr = Array.from(tourList.children)
-const gridArr = Array.from(tourContent.children)
+const freeTripList = document.querySelector('.top-list')
+const freeTripContent = document.querySelector('.free-trip-content')
 
-tourList.addEventListener('click', (e) => {
-  const target = e.target
-  if (target.nodeName == 'LI') {
-    liArr.forEach((el) => {
-      el.classList.remove('link-active')
-    })
-    target.classList.add('link-active')
+// 日本跟團 - tab 和 tab 內容陣列
+const tourTabArr = Array.from(tourList.children)
+const tourGridArr = Array.from(tourContent.children)
 
-    // 顯示 grid
-    const gridIndex = liArr.indexOf(target)
-    gridArr.forEach((el) => (el.style.display = 'none'))
-    gridArr[gridIndex].style.display = 'grid'
-  }
-})
+// 自遊最夯 - tab 和 tab 內容陣列
+const freeTripTabArr = Array.from(freeTripList.children)
+const freeTripGridArr = Array.from(freeTripContent.children)
+
+function switchTabs(list, arr, gridArr) {
+  list.addEventListener('click', (e) => {
+    const target = e.target
+    if (target.nodeName == 'LI') {
+      arr.forEach((el) => {
+        el.classList.remove('link-active')
+      })
+      target.classList.add('link-active')
+
+      // 顯示 grid
+      const gridIndex = arr.indexOf(target)
+      if (gridIndex >= 0 && gridIndex < gridArr.length) {
+        gridArr.forEach((el) => (el.style.display = 'none'))
+        gridArr[gridIndex].style.display = 'grid'
+      }
+      console.log(target);
+    }
+  })
+}
+
+switchTabs(tourList, tourTabArr, tourGridArr)
+switchTabs(freeTripList, freeTripTabArr, freeTripGridArr)
+
 function init() {
-  liArr[0].classList.add('link-active')
+  tourTabArr[0].classList.add('link-active')
+  freeTripTabArr[0].classList.add('link-active')
 }
 init()
